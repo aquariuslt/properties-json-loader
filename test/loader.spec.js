56c04804262063456c57397121e69c4aafa831b6
Aquariuslt <superaquariuslt@gmail.com>
2018-09-16 18:29:07 +0800

    [Refactor] Rename .test to .spec cause using bdd style.

--- test/loader.spec.js
+++ test/loader.spec.js
@@ -2,7 +2,7 @@ import compiler from './compiler';
 
 describe('properties-json-loader', () => {
 
-  it('# should default options convert properties to flatten json', async () => {
+  it('# should default options convert properties to flatten json.', async () => {
     const stats = await compiler('fixtures/sample.properties');
     const output = stats.toJson().modules[0].source;
 
