1b1ef5ce259f11b2568206de9dbf57f7be524d38
Aquariuslt <superaquariuslt@gmail.com>
2018-10-04 04:37:15 +0800

    [UT] Add error branch test cases.

--- test/loader.spec.js
+++ test/loader.spec.js
@@ -6,7 +6,16 @@ describe('properties-json-loader', () => {
     const stats = await compiler('fixtures/sample.properties');
     const output = stats.toJson().modules[0].source;
 
-    const EXPECT_OUTPUT = `module.exports = {"test":{"key":"Test Key Label"}}`;
+    const EXPECT_OUTPUT = `module.exports = {"test":{"key":"Test key label"}}`;
+
+    expect(output).toBe(EXPECT_OUTPUT);
+  });
+
+  it('# should log error when parse non-well formatted properties file.', async () => {
+    const stats = await compiler('fixtures/sample-error.properties');
+    const output = stats.toJson().modules[0].source;
+
+    const EXPECT_OUTPUT = `module.exports = undefined`;
 
     expect(output).toBe(EXPECT_OUTPUT);
   });
